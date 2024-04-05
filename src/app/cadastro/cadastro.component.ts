e22e4fa95ab482a8aa54932df0057cb4540800a0
julien-vaz <jhmvaz95@gmail.com>
2024-04-05 20:03:00 -0300

    Tela de cadastro implementada

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
+  users = localStorage.getItem("users");
 
+  signUpForm!: FormGroup;
+
+  constructor(private router: Router){}
+
+  createForm() {
+    this.signUpForm = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      birthdate: new FormControl(''),
+      password: new FormControl(''),
+      repeatPassword: new FormControl('')
+    })
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  signUp() {
+    if (!this.signUpForm.valid){
+      alert("Existem campos vazios!");
+      return;
+    }
+    const user = {
+      name: this.signUpForm.value.name,
+      email: this.signUpForm.value.email,
+      birthdate: this.signUpForm.value.birthdate,
+      password: this.signUpForm.value.password,
+      repeatPassword: this.signUpForm.value.repeatPassword
+    };
+    if (this.signUpForm.value.password === this.signUpForm.value.repeatPassword) {
+      if (this.users) {
+        if (!this.users.includes(String(this.signUpForm.value.email))) {
+          this.users += JSON.stringify(user);
+          console.log(this.users);
+          localStorage.setItem("users", this.users);
+        }
+      } else {
+        localStorage.setItem("users", JSON.stringify(user));
+      }
+    } else {
+      alert("A senha e sua confirmação não são iguais!");
+    }
+  }
+
+  goToLogin(){
+    this.router.navigate(["/login"]);
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  users = localStorage.getItem("users");
+  arrayUsers: JSON | undefined;
 
-}
+  signInForm!: FormGroup;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.signInForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl('')
+    })
+  }
+
+  signIn() {
+    if (this.users != null) {
+      this.arrayUsers = JSON.parse(this.users);
+      // TODO: validação de login
+    } else {
+      alert("Email não cadastrado!");
+    }
+  }
+
+  resetPassword(){
+    console.log(this.users);
+    this.createForm();
+    this.signInForm.value.password = "a1b2c4d4";
+    // TODO: enviar dados ao localStorage
+  }
+
+  goToSignUp(){
+    this.router.navigate(["/cadastro"]);
+  }
+
+};
